8bcfc1646fa329132b27611b80a8ed7dd7510a76
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-16 15:52:33 +0200

    oh

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/reuzen/{slug}', function ($slug) {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
         'reuzen' => $reuzen,
-        'active' => $reus,
+        'reus' => $reus,
 
     ]);
 })->name('reus');
