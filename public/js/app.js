b3387a25e1c3d7aef472e0714251d5cf017ef8bf
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-16 15:07:29 +0200

    da

--- public/js/app.js
+++ public/js/app.js
@@ -28887,7 +28887,7 @@ var _hoisted_1 = {
 var _hoisted_2 = ["src"];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("img", {
-    src: _ctx.data - _ctx.image,
+    src: this.data - _ctx.image,
     "class": "w-full xl:w-5/6 xl:-mt-24 mx-auto"
   }, null, 8
   /* PROPS */
