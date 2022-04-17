5bfaad031febb1d8ab4e695cca8b51ae2ee34bbf
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-17 02:06:28 +0200

    uit

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -59,7 +59,7 @@
                 <div class="flex flex-col md:flex-row my-16">
                     <img src="/images/fiere.jpeg" class="block w-full md:w-1/2 lg:w-1/3 h-full">
                     <div class="flex-col px-8">
-                        <p class="text-5xl font-extrabold my-4 text-gray-700">U wordt verwacht op</p>
+                        <p class="text-5xl font-extrabold my-4 text-gray-700">U bent uitgenodigd op</p>
                         <p class="text-3xl font-extrabold my-4">Zaterdag 21 mei - 11u</p>
                         <p class="text-2xl font-extrabold mt-4">In de historische binnenstad van Leuven</p>
                         <p>De optocht vertrekt om 11 uur op het <strong>Ladeuzenplein</strong>. Eindhalte op de Werelfeesten in het <strong>Bruulpark</strong>.</p>
