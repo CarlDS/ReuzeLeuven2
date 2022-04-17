053f6444998cf9c5d3448bc04e633b4d47932038
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-17 23:44:02 +0200

    full

--- resources/js/ReuzeLeuven/ReuzenBier.vue
+++ resources/js/ReuzeLeuven/ReuzenBier.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class=" text-center md:text-left text-4xl font-bold p-4 text-gray-700">Reuzebier</h1>
-    <div class="flex flex-col-reverse md:flex-row px-4">
-        <img src="/images/reuzenbier.jpeg" class="">
+    <div class="flex flex-col md:flex-row px-4">
+        <img src="/images/reuzenbier.jpeg" class="w-1/2 md:w-full">
         <div class="">
             <h2 class="text-3xl font-bold py-2">Fris bier met een uniek karakter</h2>
             <h3 class="text-xl font-bold">Fris sterk blond</h3>
@@ -11,7 +11,7 @@
             <h3 class="text-xl font-bold">Reuzelekker gehopt</h3>
             <p class="pb-1">Gerst en tarwe als brouwgranen zorgen voor een zacht en volmondig bier. Zijn typische smaak heeft het reuzebier echter te danken aan twee Amerikaanse aromahoppen “mosaic” en “centennial”. De gebruikte hopsoorten hebben trouwens ook een symbolische betekenis. “Centennial” verwijst naar de eeuwenlange reuzentraditie. “Mosaic” staat dan weer voor de diversiteit die de reuzenstoet hoog in het vaandel draagt. De combinatie van deze twee hoppen zorgt voor een toegankelijk, fris bier met een uniek karakter. Ideaal om te drinken in een heerlijk zonnetje.</p>
             <div class="w-1/4 mx-auto my-4">
-                <a href="http://www.luvanium.be/reuzebier.html" target="_blank"><img src="/images/logo3_luvanium.png" class="h-40"></a>
+            <a href="http://www.luvanium.be/reuzebier.html" target="_blank"><img src="/images/logo3_luvanium.png" class="h-40"></a>
             </div>
         </div>
     </div>
