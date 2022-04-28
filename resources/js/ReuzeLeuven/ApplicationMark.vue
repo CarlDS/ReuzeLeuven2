983ec1cfac381da506dbe780ab1fea6f1280d314
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-28 21:47:44 +0200

    LK

--- resources/js/ReuzeLeuven/ApplicationMark.vue
+++ resources/js/ReuzeLeuven/ApplicationMark.vue
@@ -1,5 +1,5 @@
 <template>
-<div class=" bg-white p-2">
-    <img src="/images/RLLogo.png" class="" alt="">
+<div class="rounded-full bg-white p-2">
+    <img src="/images/RLLogo.png" class="h-full w-full" alt="">
 </div>
 </template>
