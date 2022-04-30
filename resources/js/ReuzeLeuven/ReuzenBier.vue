0ad3bda639eca2df902bf3eadc41e242bdb933b7
CPMDesmedt <carl.desmedt@icloud.com>
2022-04-30 12:12:57 +0200

    lm

--- resources/js/ReuzeLeuven/ReuzenBier.vue
+++ resources/js/ReuzeLeuven/ReuzenBier.vue
@@ -7,7 +7,7 @@
             <h3 class="text-xl font-bold">Fris sterk blond</h3>
             <p class="pb-1">Dit fris, sterk blond bier combineert de oude Leuvense brouwgranen met de nieuwe buitenlandse hopsoorten en is daarom een heerlijke mengeling van oude tradities met nieuwe culturen. Het resultaat is een uniek, zacht, blond bier van 8,5% vol. alc. met een lekkere toets van tropisch fruit en citrus.</p>
             <h3 class="text-xl font-bold">Reuzeleuven</h3>
-            <p class="pb-1">Luvanium Reuzebier is een samenwerking van Luvanium met de vzw Reuzeleuven. Het bier werd gebrouwen ter ere van de talrijke reuzen in Leuven en ter gelegenheid van de stoet Reuzeleuven die verplaatst is naar het voorjaar van 202 (in plaats van mei 2020 zoals oorspronkelijk gepland).</p>
+            <p class="pb-1">Luvanium Reuzebier is een samenwerking van Luvanium met de vzw Reuzeleuven. Het bier werd gebrouwen ter ere van de talrijke reuzen in Leuven en ter gelegenheid van de stoet Reuzeleuven die verplaatst is naar mei 2022 (in plaats van mei 2020 zoals oorspronkelijk gepland).</p>
             <h3 class="text-xl font-bold">Reuzelekker gehopt</h3>
             <p class="pb-1">Gerst en tarwe als brouwgranen zorgen voor een zacht en volmondig bier. Zijn typische smaak heeft het reuzebier echter te danken aan twee Amerikaanse aromahoppen “mosaic” en “centennial”. De gebruikte hopsoorten hebben trouwens ook een symbolische betekenis. “Centennial” verwijst naar de eeuwenlange reuzentraditie. “Mosaic” staat dan weer voor de diversiteit die de reuzenstoet hoog in het vaandel draagt. De combinatie van deze twee hoppen zorgt voor een toegankelijk, fris bier met een uniek karakter. Ideaal om te drinken in een heerlijk zonnetje.</p>
             <div class="w-1/4 mx-auto my-4">
